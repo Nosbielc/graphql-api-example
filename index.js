ac2f26e41c762527125fd8b820890a7cfca961c6
Cleibson Gomes <cleibson.gomes.silva@gmail.com>
2020-01-08 21:57:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,69 @@ const { ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
     scalar Date
     
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+    
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float!
+    }
+    
     # Pontos de entrada da API!
     type Query {
       ola : String
-      horaCerta : Date  
+      horaCerta : Date
+      usuarioLogado : Usuario
+      produtoEmDestaque : Produto
     }
 `
 
 const resolvers = {
-    Query : {
+
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
+        }
+    },
+    Produto: {
+        precoComDesconto(produto) {
+            if (produto.desconto)
+                return produto.preco * (1 - produto.desconto)
+            return produto.preco
+        }
+    },
+
+    Query: {
         ola() {
             return 'String de retorno'
         },
         horaCerta() {
           return new Date
+        },
+        usuarioLogado(){
+            return {
+                id: 1,
+                nome: "Administrador",
+                email: "admin@gmail.com",
+                idade: 47,
+                salario_real: 1234.98,
+                vip: true
+            }
+        },
+        produtoEmDestaque() {
+            return {
+                nome: "Camisa Oficial do Ibis FC 2020",
+                preco: 8997.00,
+                desconto: 0.50
+            }
         }
     }
 }
